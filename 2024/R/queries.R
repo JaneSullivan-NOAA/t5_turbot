5628c220beedf6b3c1ae5f891e4f69e9e43591c4
JaneSullivan-NOAA <jane.sullivan@noaa.gov>
2024-12-03 18:36:01 -0900

    files for t5 alt in 2024

---
+++ 2024/R/queries.R
@@ -0,0 +1,71 @@
+# Queries for a Tier 5 version of the Greenland turbot assessment
+
+# survey_definition_id
+# 98 = EBS shelf
+# 52 = AI
+# 78 = EBS slope
+
+# Set up ----
+
+libs <- c("odbc", "keyring", "dplyr", "dbplyr", "tidyr", "lubridate", "readr", "here", "ggplot2")
+if(length(libs[which(libs %in% rownames(installed.packages()) == FALSE )]) > 0) {install.packages(libs[which(libs %in% rownames(installed.packages()) == FALSE)])}
+lapply(libs, library, character.only = TRUE)
+
+yr <- 2024 # assessment yr
+dir.create(here(yr))
+dir.create(here(yr, "data"))
+dir.create(here(yr, "data", "raw"))
+
+race_spp <- 10115 # race species code for turbot
+
+akfin <- DBI::dbConnect(odbc::odbc(), "akfin", uid = keyring::key_list("akfin")$username,
+                        pwd = keyring::key_get("akfin", keyring::key_list("akfin")$username))
+
+# BTS ----
+
+lkup <- tbl(akfin, sql("gap_products.akfin_area")) %>%
+  rename_all(tolower) %>% 
+  filter(survey_definition_id %in% c(52, 98, 78) & area_type == "REGION") %>% 
+  collect() 
+
+# my_ids <- lkup %>% pull(area_id)
+my_ids <- c(99901, # EBS shelf standard grid
+            99904, # AI
+            99905  # EBS slope
+)
+
+biom <- tbl(akfin, sql("gap_products.akfin_biomass")) %>%
+  rename_all(tolower) %>%
+  filter(species_code %in% race_spp & area_id %in% my_ids) %>%
+  select(survey_definition_id, year, biomass_mt, biomass_var, n_haul, n_count) %>%
+  collect() %>%
+  mutate(common_name = "Greenland turbot",
+         survey = ifelse(survey_definition_id == 98, "EBS Shelf",
+                         ifelse(survey_definition_id == 52, "AI",
+                                "EBS Slope")),
+         cv = sqrt(biomass_var)/biomass_mt) %>%
+  arrange(survey, year) %>%
+  select(survey, survey_definition_id, common_name, year, 
+         biomass = biomass_mt, cv, n_haul, n_count)
+
+write_csv(biom, here(yr, "data", "raw", "bts_biomass_turbot.csv"))
+
+# LLS ----
+
+lls <- tbl(akfin, sql("afsc.lls_fmp_subarea_all_strata")) %>%
+  rename_all(tolower) %>%
+  filter(year >= 1996 &
+           country == "United States" &
+           species_code %in% race_spp & 
+           council_management_area %in% c("Aleutians", "Bering Sea")) %>%
+  collect() %>% 
+  filter(c(council_management_area %in% c("Bering Sea") & year %% 2 == 1) |
+           c(council_management_area %in% c("Aleutians") & year %% 2 == 0)) %>%
+  mutate(strata = ifelse(council_management_area == "Bering Sea", "EBS Slope", "AI"),
+         rpn_cv = sqrt(rpn_var)/rpn,
+         rpw_cv = sqrt(rpw_var)/rpw) %>% 
+  arrange(year, strata) %>% 
+  select(strata, year, rpn, rpn_cv, rpw, rpw_cv)
+lls %>% print(n=Inf)
+
+write_csv(lls, here(yr, "data", "raw", "lls_turbot.csv"))
---
+++ 2024/R/rema.R
@@ -0,0 +1,80 @@
+# Tier 5 version of the Greenland turbot assessment
+
+# set up ----
+libs <- c("dplyr", "tidyr", "readr", "here", "ggplot2")
+if(length(libs[which(libs %in% rownames(installed.packages()) == FALSE )]) > 0) {install.packages(libs[which(libs %in% rownames(installed.packages()) == FALSE)])}
+lapply(libs, library, character.only = TRUE)
+
+# devtools::install_github("afsc-assessments/rema")
+library(rema)
+
+ggplot2::theme_set(cowplot::theme_cowplot(font_size = 10) +
+                     cowplot::background_grid() +
+                     cowplot::panel_border())
+
+yr <- 2024 # assessment yr
+dir.create(here(yr))
+dir.create(here(yr, "results"))
+
+# analysis ----
+
+bts <- read_csv(here(yr, "data", "raw", "bts_biomass_turbot.csv")) %>% 
+  select(strata = survey, year, biomass, cv)
+lls <- read_csv(here(yr, "data", "raw", "lls_turbot.csv")) %>% 
+  # choice to be made here about whether to use RPNs or RPWs. I went with RPNs
+  # because that's what are currently used
+  mutate(strata, year, cpue = rpn, cv = rpn_cv)
+  # mutate(strata, year, cpue = rpw, cv = rpw_cv)
+
+sort(unique(bts$strata))
+input <- prepare_rema_input(model_name = 'Tier 5 turbot',
+                            multi_survey = 1, # this is 1 because we want to use the LLS information
+                            biomass_dat = bts, 
+                            cpue_dat = lls,
+                            start_year = 1991,
+                            end_year = yr+1,
+                            sum_cpue_index = 1, # this is 1 because LLS RPN/RPWs are summable 
+                            # specify shared/pooled process error across the 3
+                            # strata (default is c(1,2,3))
+                            PE_options = list(pointer_PE_biomass = c(1, 1, 1)),
+                            # LLS only available in AI and on EBS slope.
+                            # estimate separate q's for these and specify EBS
+                            # shelf as NA
+                            q_options = list(pointer_biomass_cpue_strata = c(1, NA, 2)))
+mod1 <- fit_rema(input)
+saveRDS(mod1, here::here(yr, "results", "t5_turbot.rds"))
+out <- tidy_rema(mod1)
+# create empty placeholder so that strata plot panels are aligned for the
+# BTS/LLS
+out$cpue_by_strata <- out$cpue_by_strata %>% 
+  bind_rows(data.frame(strata = "EBS Shelf",
+                       year = unique(out$cpue_by_strata$year)))
+plots <- plot_rema(out, cpue_ylab = 'RPN', biomass_ylab = 'Biomass (t)')
+p1 <- plots$biomass_by_strata + facet_wrap(~strata, nrow = 1) + ggtitle('Bottom trawl survey')
+p2 <- plots$cpue_by_strata + facet_wrap(~strata, nrow = 1) + ggtitle('Longline survey')
+p3 <- plots$total_predicted_biomass + 
+  scale_y_continuous(labels = scales::comma, expand = c(0.02, 0), limits = c(0, NA)) +
+  ggtitle("Total predicted biomass")
+cowplot::plot_grid(p1, p2, p3, ncol = 1)
+ggsave(here(yr, "results", "t5_turbot.png"), units = 'in', bg = 'white',
+       height = 9.5, width = 8, dpi = 300)
+
+natmat <- 0.112 # from 2024 assessment
+specs <- out$total_predicted_biomass %>% 
+  filter(year == yr+1) %>% 
+  mutate(ofl = round(natmat * pred, 0),
+         abc = round(natmat * 0.75 * pred, 0))
+glimpse(specs)
+# $ model_name <chr> "Tier 5 turbot"
+# $ variable   <chr> "tot_biomass_pred"
+# $ year       <int> 2025
+# $ pred       <dbl> 14776.7
+# $ pred_lci   <dbl> 8848.229
+# $ pred_uci   <dbl> 24677.36
+# $ ofl        <dbl> 1655
+# $ abc        <dbl> 1241
+
+# 2025 Tier specs from the 2024 assessment:
+# OFL = 2598 t
+# maxABC = 2237 t
+# ABC = 2013 t
